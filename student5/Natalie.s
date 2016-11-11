88ded55ed5de98adaa2f776c97808ab531606049
glendale-unified-coding <glendale.unified.coding@gmail.com>
2016-11-10 16:11:12 -0800

    Create Natalie.s

---
+++ student5/Natalie.s
@@ -0,0 +1,29 @@
+<html>
+<head>
+
+<style>
+	.pizza {
+		color:red;
+	}
+	#lonely {
+		color:blue;
+	}
+	body {
+		border-style: solid;
+		border-color: red;
+		border-width: medium;
+		background-image: url("http://cbsnews3.cbsistatic.com/hub/i/r/2016/03/23/38e32f54-b910-4612-8852-be9e0fbdbf73/thumbnail/620x350/440a1273973991f75a0ac768f554e37c/cat-istock.jpg");
+		background-repeat: repeat;
+		background-posistion: right;
+		margin:200px;
+	}
+</style>
+</head>
+
+<body>
+	<div class="pizza"> Im a div tag!</div>
+	<div class="pizza"> Same!</div>
+	<div id="lonely">How about me?</div>
+</body>
+
+</html>
